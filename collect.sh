21b86691d87512770594ff5daade80d66d5da237
chaowei <chaowei>
2026-03-04 19:59:30 +0800

    submit

--- collect.sh
+++ collect.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+source install/setup.bash 
+
 DATASET_DIR="/home/agilex/data/sss"
 TASK_NAME="123"
 MAX_STEPS=4000
 USE_BASE_ROBOT=False
 LANGUAGE_RAW="Do folds on the shorts."
 
-source ~/miniconda3/bin/activate
-conda activate aloha
-cd collect_data
+# source ~/miniconda3/bin/activate
+# conda activate aloha
+# cd collect_data
 
 mkdir -p $DATASET_DIR/$TASK_NAME
 
@@ -22,6 +24,7 @@ fi
 
 next_num=$((max_num+1))
 
+cd collect_data
 python3 collect_data_four_camera.py --dataset_dir $DATASET_DIR --task_name $TASK_NAME --max_timesteps $MAX_STEPS --episode_idx $next_num --language_raw "$LANGUAGE_RAW"
 
 
--- start_robot.sh
+++ start_robot.sh
@@ -1,2 +1,4 @@
 source install/setup.bash
-ros2 launch piper start_two_piper.launch.py can_left_port:=can_arm1 can_right_port:=can_arm2 auto_enable:=false girpper_exist:=true
+bash can_muti_activate.sh &
+ros2 launch piper start_two_piper.launch.py can_left_port:=can_arm1 can_right_port:=can_arm2 auto_enable:=false girpper_exist:=true &
+
